b9a1a1c3d32ad628c20681863d20719748fceb68
LeggoTM <m.tanmay17@gmail.com>
2020-12-23 18:32:01 +0530

    Created Register Page

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,21 @@ var config = require('../config');
 var transporter = nodemailer.createTransport(config.mailer);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'CodeDuo - A platform for sharing code.' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'CodeDuo - A platform for sharing code.'});
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'CodeDuo - A platform for sharing code.' });
 });
 
 router.route('/contact')
-  .get(function(req, res, next) {
-    res.render('contact', { title: 'CodeDuo - A platform for sharing code.'});
+  .get(function (req, res, next) {
+    res.render('contact', { title: 'CodeDuo - A platform for sharing code.' });
   })
-  .post(function(req, res, next) {
-    
-    
+  .post(function (req, res, next) {
+
+
 
     var mailOptions = {
       from: 'CodeDuo <no-reply@codeduo.com>',
@@ -29,15 +29,24 @@ router.route('/contact')
       text: req.body.message,
     }
 
-    transporter.sendMail(mailOptions, function(error, info) {
-      if(error) {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
         return console.log(error);
       }
-      res.render('thank', { title: 'CodeDuo - A platform for sharing code.'});
+      res.render('thank', { title: 'CodeDuo - A platform for sharing code.' });
     })
 
 
-    
+
   });
+
+
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Login your account' });
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register', { title: 'Registe a new account' });
+});
 
 module.exports = router;
