2219ce7cdf7cd302f03c6afdcfcc99079eca26cd
LeggoTM <m.tanmay17@gmail.com>
2020-12-23 10:34:58 +0530

    Added contact us page

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -37,5 +36,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', {title: 'CodeDuo - A platform for sharing code.'});
+  res.render('about', { title: 'CodeDuo - A platform for sharing code.'});
 });
+
+router.route('/contact')
+  .get(function(req, res, next) {
+    res.render('contact', { title: 'CodeDuo - A platform for sharing code.'});
+  })
+  .post(function(req, res, next) {
+    res.render('thank', { title: 'CodeDuo - A platform for sharing code.'});
+  });
 
 module.exports = router;
